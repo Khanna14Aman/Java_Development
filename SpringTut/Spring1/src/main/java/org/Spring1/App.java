da8d11586421386acdbdb4519790fba028054fdf
Khanna <aman.khanna02@sap.com>
2025-12-16 21:14:19 +0530

    Spring1

---
+++ SpringTut/Spring1/src/main/java/org/Spring1/App.java
@@ -0,0 +1,20 @@
+package org.Spring1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("Spring.xml");
+        Student student1 = (Student) applicationContext.getBean("student");
+        student1.coding();
+        Student student2 = (Student) applicationContext.getBean("student");
+        student2.coding();
+    }
+}
---
+++ SpringTut/Spring1/src/main/java/org/Spring1/Student.java
@@ -0,0 +1,10 @@
+package org.Spring1;
+
+public class Student {
+    public Student(){
+        System.out.println("Student Created");
+    }
+    public void coding(){
+        System.out.println("Codding");
+    }
+}
